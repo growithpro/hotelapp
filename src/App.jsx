e7d1c4177f334fed4ff0ba50e23eb818c69b6179
Naveen Joshi <naveen.joshi.net@gmail.com>
2022-12-15 15:26:19 +0530

    Add pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home/>,
+    },
+    {
+      path: "/profile",
+      element: <Profile/>,
+    },
+    {
+      path: "/signin",
+      element: <SignIn/>,
+    },
+    {
+      path: "/signup",
+      element: <SignUp/>,
+    },
+    {
+      path: "/forgot-password",
+      element: <ForgotPassword/>,
     },
   ]);
   return (
